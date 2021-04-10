6dcf1e74d520797465bbdc32d703f20950bbecd4
Angus Enchieh Yang <yangus1265@users.noreply.github.com>
2021-04-10 13:35:49 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -117,8 +117,7 @@ def ping(host, timeout=1):
     # timeout=1 means: If one second goes by without a reply from the server,  	
     # the client assumes that either the client's ping or the server's pong is lost
     dest = gethostbyname(host)
-    print("Pinging " + dest + " using Python:")
-    print("")
+   
     # Calculate vars values and return them
     #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
@@ -127,10 +126,8 @@ def ping(host, timeout=1):
     response = []
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
-        print(delay)
         time.sleep(1)  # one second
         response.append(delay)
-        print(f'end of {i}')
     # calculating the stats
     
     packet_min = min(response)*1000
